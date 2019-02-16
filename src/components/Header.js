a00b2f628014d0f7c6174142c5a2106f62d47491
Lee Blundell <leeblundell16@gmail.com>
2019-02-16 20:42:40 +0000

    add logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,27 +1,37 @@
-import React from 'react';
-import { BrowserRouter as Router, Link, Route } from 'react-router-dom';
-import Home from '../pages/Home';
+import React from "react";
+import { BrowserRouter as Router, Link, Route } from "react-router-dom";
+import logo from "../assets/logo.svg";
+import Home from "../pages/Home";
 
 const About = () => (
   <div>
     <h2>About</h2>
   </div>
-)
+);
 
 const Header = () => (
   <Router>
-    <div>
-      <ul>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/about">About</Link></li>
-      </ul>
+    <>
+      <header>
+        <img src={logo} alt="" />
+      </header>
+      <div>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+        </ul>
 
-      <hr/>
+        <hr />
 
-      <Route exact path="/" component={Home}/>
-      <Route path="/about" component={About}/>
-    </div>
+        <Route exact path="/" component={Home} />
+        <Route path="/about" component={About} />
+      </div>
+    </>
   </Router>
-)
+);
 
 export default Header;
